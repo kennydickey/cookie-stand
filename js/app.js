aa3eb786333b8fdf00366f43a8407041a59792e0
seatacken <57050116+seatacken@users.noreply.github.com>
2019-12-10 07:11:09 -0800

    cookie stand loctions refactor

--- js/app.js
+++ js/app.js
@@ -1,56 +1,68 @@
 'use strict';
 console.log('js is working');
 
+//find the product of average cookies per person * people per hour
+var cookiesPerHourCalc = function(){
+  var perHour = (this.avgCookiesPerCust * this.averageCust());
+  console.log(perHour);
+  return perHour;
+};
+//find average custome count per hour
+var averageCustCalc = function(){
+  var randomCalc = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  console.log(randomCalc);
+  return randomCalc;
+};
 
 var seattle = {
-  //hourly customers
   minCust : 23,
   maxCust : 65,
   avgCookiesPerCust : 6.5,
-  randomCust : function(){
-    var random = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    console.log(random);
-    return random;
-  },
-  cookiesPerHour : function(){
-    var cookies = (this.avgCookiesPerCust * this.randomCust());
-    console.log(cookies);
-    return cookies;
-  }
-
+  //average customer moved to a var
+  averageCust : averageCustCalc,
+  //average customer times fixed cookies per hour average moved to a var
+  cookiesPerHour : cookiesPerHourCalc
 };
-seattle.randomCust();
-seattle.cookiesPerHour();
-
-// seattle.cookiesCalc();
-
-seattle.cookiesPerHour();
-
-
-
-
-
 var tokyo = {
   //hourly customers
   minCust : 3,
   maxCust : 24,
-  avgCookiesPerCust : 1.2
+  avgCookiesPerCust : 1.2,
+  averageCust : averageCustCalc,
+  cookiesPerHour : cookiesPerHourCalc
 };
 var dubai = {
   //hourly customers
   minCust : 11,
   maxCust : 38,
-  avgCookiesPerCust : 3.7
+  avgCookiesPerCust : 3.7,
+  averageCust : averageCustCalc,
+  cookiesPerHour : cookiesPerHourCalc
 };
 var paris = {
   //hourly customers
   minCust : 20,
   maxCust : 38,
-  avgCookiesPerCust : 2.3
+  avgCookiesPerCust : 2.3,
+  averageCust : averageCustCalc,
+  cookiesPerHour : cookiesPerHourCalc
 };
 var lima ={
   //hourly customers
   minCust : 2,
   maxCust : 16,
-  avgCookiesPerCust : 4.6
+  avgCookiesPerCust : 4.6,
+  averageCust : averageCustCalc,
+  cookiesPerHour : cookiesPerHourCalc
 };
+
+seattle.averageCust();
+seattle.cookiesPerHour();
+tokyo.averageCust();
+tokyo.cookiesPerHour();
+dubai.averageCust();
+dubai.cookiesPerHour();
+paris.averageCust();
+paris.cookiesPerHour();
+lima.averageCust();
+lima.cookiesPerHour();
